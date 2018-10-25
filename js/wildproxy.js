8fde551373f8d07a57ddbb0f15c2e62c7a8f0847
Антон Степаненков <Lenseg1@gmail.com>
2018-10-25 18:20:10 +0200

    history polyfill

--- js/wildproxy.js
+++ js/wildproxy.js
@@ -65,8 +65,16 @@
   /**
    * History API CORS errors stubbing with window monkey patching
    */
-  if (window.history) {
-    window.history.pushState = silentWrapper(window.history.pushState)
-    window.history.replaceState = silentWrapper(window.history.replaceState)
-  }
+   if (window.history) {
+     const pushState = window.history.pushState.bind(window.history),
+     replaceState = window.history.replaceState.bind(window.history);
+     history.pushState = function(state, title, url) {
+       const prependUrl = origin + '/' + targetURL + url;
+       pushState(state, title, prependUrl)
+     }
+     history.replaceState = function(state, title, url) {
+       const prependUrl = origin + '/' + targetURL + url;
+       replaceState(state, title, prependUrl)
+     }
+   }
 })();
