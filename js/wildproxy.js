13819b00e77aa85d07b6110dbbbcc13e89c573ea
m-russkikh <vyatich23-61@yandex.ru>
2018-10-26 14:25:53 +0300

    Fix resource url in window.open method

--- js/wildproxy.js
+++ js/wildproxy.js
@@ -43,6 +43,13 @@ import parseUrl from 'url-parse';
     });
   }
 
+  // Fix resource url in window.open method
+  const originalWindowOpen = window.open;
+
+  window.open = (url, windowName, windowFeatures) => {
+    return originalWindowOpen(prepareUrl(url), windowName, windowFeatures);
+  };
+
   function prepareUrl(url) {
     const parsedUrl = parseUrl(url);
 
