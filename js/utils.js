58fe23ecda9ee7f735297762c383ef2df4c1458a
m-russkikh <vyatich23-61@yandex.ru>
2018-10-31 11:36:31 +0300

    Fix prepareUrl util

--- js/utils.js
+++ js/utils.js
@@ -2,17 +2,11 @@ import parseUrl from 'url-parse';
 
 import { TARGET_URL, ORIGIN_URL } from './constants';
 
-const parsedTargetUrl = parseUrl(TARGET_URL);
-
 export const prepareUrl = url => {
   const parsedUrl = parseUrl(url);
 
   if (/^http/.test(url)) {
-    if (parsedUrl.origin !== ORIGIN_URL && parsedUrl.hostname.includes(parsedTargetUrl.hostname)) {
-      return `${ORIGIN_URL}/${url}`;
-    } else {
-      return url;
-    }
+    return parsedUrl.origin === origin ? url : `${origin}/${url}`;
   } else {
     const withoutLeadingSlash = url[0] !== '/';
     let result = ORIGIN_URL;
