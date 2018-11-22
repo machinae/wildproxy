3431e971ee0adcc698ce87b7585f55e0e26f8af1
m-russkikh <vyatich23-61@yandex.ru>
2018-11-22 17:26:52 +0300

    Fix origin in message event

--- js/postMessage.js
+++ js/postMessage.js
@@ -1,10 +1,15 @@
-// Fix targetOrigin in window.postMessage method
+import { TARGET_URL } from './constants';
+
 const originalWindowPostMessage = window.postMessage;
+const getData = message => ({
+  message,
+  origin: TARGET_URL,
+});
 
 window.postMessage = (message, targetOrigin, transfer) =>
-  originalWindowPostMessage(message, '*', transfer);
+  originalWindowPostMessage.call(window, getData(message), '*', transfer);
 
 if (window.parent) {
   window.parent.postMessage = (message, targetOrigin, transfer) =>
-    originalWindowPostMessage(message, '*', transfer);
+    originalWindowPostMessage.call(window.parent, getData(message), '*', transfer);
 }
